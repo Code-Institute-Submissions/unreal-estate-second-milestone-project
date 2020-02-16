4c6f89ed5ba371d5540edf667ce4f25494cbed08
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-02-16 12:37:52 +0000

    Empyt canvas added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,16 @@
-console.log("hello world")
 
+//Calling a function to start the game:
+function startGame() {
+  myGameArea.start();
+}
+
+// Game area:
+var myGameArea = {
+  canvas : document.createElement("canvas"),
+  start : function() {
+    this.canvas.width = 580;
+    this.canvas.height = 470;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+  }
+}
