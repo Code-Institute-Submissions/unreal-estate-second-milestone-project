b748cb28a1ae7dfb34b1f459cdd3b6d4189c7944
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-02-18 18:16:17 +0000

    Game added, canvas imported

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,106 @@
-//Calling a function to start the game:
-function startGame() {
-  myGameArea.start();
+var canvas;
+var ctx;
+var dx = 5;
+var dy = 5;
+var x = 200;
+var y = 5;
+var WIDTH = 482;
+var HEIGHT = 482;
+var img = new Image();
+var collision = 0;
+
+function rect(x,y,w,h) {
+ctx.beginPath();
+ctx.rect(x,y,w,h);
+ctx.closePath();
+ctx.fill();
 }
 
-// Game area:
-var myGameArea = {
-  canvas : document.createElement("canvas"),
-  start : function() {
-    this.canvas.width = 580;
-    this.canvas.height = 470;
-    this.context = this.canvas.getContext("2d");
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-  }
+function clear() {
+ctx.clearRect(0, 0, WIDTH, HEIGHT);
+ctx.drawImage(img, 0, 0);
+}
+
+function init() {
+canvas = document.getElementById("canvas");
+ctx = canvas.getContext("2d");
+img.src = "maze.gif";
+return setInterval(draw, 10);
+}
+
+function doKeyDown(evt){
+switch (evt.keyCode) {
+case 38:  /* Up arrow was pressed */
+if (y - dy > 0){
+y -= dy;
+clear();
+checkcollision();
+if (collision == 1){
+y += dy;
+collision = 0;
+}
+}
+
+break;
+case 40:  /* Down arrow was pressed */
+if (y + dy < HEIGHT ){
+y += dy;
+clear();
+checkcollision();
+if (collision == 1){
+y -= dy;
+collision = 0;
+}
+}
+
+break;
+case 37:
+if (x - dx > 0){
+x -= dx;
+clear();
+checkcollision();
+if (collision == 1){
+x += dx;
+collision = 0;
+}
+}
+break;
+case 39:
+if ((x + dx < WIDTH)){
+x += dx;
+clear();
+checkcollision();
+if (collision == 1){
+x -= dx;
+collision = 0;
+}
+}
+break;
+}
+}
+
+function checkcollision() {
+var imgd = ctx.getImageData(x, y, 15, 15);
+var pix = imgd.data;
+for (var i = 0; n = pix.length, i < n; i += 4) {
+if (pix[i] == 0) {
+collision = 1;
+}
+}
+}
+
+function draw() {
+clear();
+ctx.fillStyle = "purple";
+rect(x, y, 15,15);
+}
+
+init();
+window.addEventListener('keydown',doKeyDown,true);
+
+function init() {
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  img.src = "assets/images/maze.gif"; 
+  return setInterval(draw, 10);
 }
