f44c73db86756f7ed1accabb5a907f7c45531621
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-02-19 19:11:47 +0000

    drawing context added to canvas

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,19 @@
-// Create the canvas, drawing context added 
-var x = document.createElement("CANVAS");
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-console.log(ctx);
+// Creating the maze, holding the data
+var board = new Array();
+ 
+    function maze() {
+        init();           
+        explorePath(1,1); }
+// Initalizing the canvas, drawing context added, filling the array with zeros
+function init() {
+        var canvas = document.getElementById("canvas");
+        if (canvas.getContext) {
+            context = canvas.getContext("2d");
+for(i=0; i< SIZE; i++) {
+            board[i] = new Array();
+            for(j=0; j<SIZE; j++) {
+                board[i][j] = 0; 
+            }
+        }
+    }
+}
