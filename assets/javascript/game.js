69549d0ada20dc514af9c4849b2ad619e7007da5
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-03-01 16:48:11 +0000

    new maze added with background colour, positioned

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,15 +7,14 @@ var currRectY = 3;
 var mazeWidth = 556;
 var mazeHeight = 556;
 var intervalVar;
-var imageWidth = 414;
-var imageHeight = 254;
+var imageWidth = 210;
+var imageHeight = 210;
 var canvasWidth = 522;
 var canvasHeight = 522;
-function drawMazeAndRectangle(rectX, rectY) {
-    makeWhite(0, 0, canvas.width, canvas.height);
+
     var mazeImg = new Image();
     mazeImg.onload = function () {
-    context.drawImage(mazeImg, 0, 0, imageWidth, imageHeight, 0, 0, canvasWidth, canvasHeight);         drawRectangle(rectX, rectY, "#0000FF", false, true);
+    context.drawImage(mazeImg, 0, 0, imageWidth, imageHeight, 0, 0, canvasWidth, canvasHeight);         
         context.beginPath();
         context.arc(542, 122, 7, 0, 2 * Math.PI, false);
         context.closePath();
@@ -24,29 +23,11 @@ function drawMazeAndRectangle(rectX, rectY) {
                
         
     };
-    mazeImg.src = "../assets/images/maze.gif";
+    mazeImg.src = "../assets/images/maze.png";
     
     
-}
 
-function drawRectangle(x, y, style) {
-    makeWhite(currRectX, currRectY, 15, 15);
-    currRectX = x;
-    currRectY = y;
-    context.beginPath();
-    context.rect(x, y, 15, 15);
-    context.closePath();
-    context.fillStyle = style;
-    context.fill();
+
+
     
-}
-function makeWhite(x, y, w, h) {
-    context.beginPath();
-    context.rect(x, y, w, h);
-    context.closePath();
-    context.fillStyle = "white";
-    context.fill();
-}
 
-
-drawMazeAndRectangle(425, 3);
