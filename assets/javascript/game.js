657f5b82d77c69bce8ea727cdb1c15ed8cd09357
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-03-03 11:01:30 +0000

    get context global scope added, player and startgame added, needed to fix

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //Initalizing the canvas, drawing context added
-var canvas = document.getElementById("mazecanvas");
-var context = canvas.getContext("2d");
+var canvas = document.getElementById("canvas");
+var context = newFunction();
 var x;
 var y;
 var currRectX = 425;
@@ -13,21 +13,104 @@ var imageHeight = 210;
 var canvasWidth = 522;
 var canvasHeight = 522;
 
-    var mazeImg = new Image();
-    mazeImg.onload = function () {
-    context.drawImage(mazeImg, 0, 0, imageWidth, imageHeight, 0, 0, canvasWidth, canvasHeight);         
-        context.beginPath();
-        context.arc(542, 122, 7, 0, 2 * Math.PI, false);
-        context.closePath();
-        context.fillStyle = '#00FF00';
-        context.fill();
+
+
+var myGamePiece;
+
+function newFunction() {
+    return getContext("2d");
+}
+
+function startGame() {
+    myGamePiece = new component(30, 30, "../assets/images/player.png", 10, 120, "image");
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas: document.getElementById("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
+        },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
+    }
+}
+
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    if (type == "image") {
+        this.image = new Image();
+        this.image.src = color;
+    }
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        context = myGameArea.context;
+        if (type == "image") {
+            context.drawImage(this.image, 
+                this.x, 
+                this.y,
+                this.width, this.height);
+        } else {
+            context.fillStyle = color;
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
+    }
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.newPos();
+    myGamePiece.update();
+}
+
+function moveup() {
+    myGamePiece.speedY = -1; 
+}
+
+function movedown() {
+    myGamePiece.speedY = 1; 
+}
+
+function moveleft() {
+    myGamePiece.speedX = -1; 
+}
+
+function moveright() {
+    myGamePiece.speedX = 1; 
+}
+
+function clearmove() {
+    myGamePiece.speedX = 0; 
+    myGamePiece.speedY = 0; 
+}
+var mazeImg = new Image();
+var playerImg = new Image();
+
+ mazeImg.onload = function () {
+context.drawImage(mazeImg, 0, 0, imageWidth, imageHeight, 0, 0, canvasWidth, canvasHeight);         
+context.beginPath();
+context.arc(542, 122, 7, 0, 2 * Math.PI, false);
+context.closePath();
+context.fillStyle = '#00FF00';
+context.fill();
                
         
-    };
-    mazeImg.src = "../assets/images/maze.png";
-var playerImg = new Image();
-playerImg.onload = function () {
-        context.drawImage(playerImg, x, y);     
-}
- playerImg.src ="../assets/images/player.png";
-      
+  };
+  mazeImg.src = "../assets/images/maze.png";
