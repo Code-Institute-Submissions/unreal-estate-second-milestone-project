ae71a2106bf2879f63349ec0301293beb7a936ef
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-03-09 12:00:46 +0000

    onload function moved, component method added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,61 @@
-//Initalizing the canvas, drawing context added
-var x;
-var y;
-var currRectX = 425;
-var currRectY = 3;
-var mazeWidth = 556;
-var mazeHeight = 556;
-var intervalVar;
-var imageWidth = 210;
-var imageHeight = 210;
-var canvasWidth = 522;
-var canvasHeight = 522;
-var context;
 var myGamePiece;
+var myBackground;
 
-function newFunction() {
-    var canvas = document.getElementById("canvas");
-    const context = canvas.getContext('2d');
-    return context;
+
+window.onload = function () {
+    startGame();
 }
 
 function startGame() {
+    console.log("did anything");
     myGamePiece = new component(30, 30, "../assets/images/player.png", 10, 120, "image");
+    myBackground = new component(656, 270, "../assets/images/maze.png", 0, 0, "background");
     myGameArea.start();
 }
 
 var myGameArea = {
-    canvas: document.getElementById("canvas"),
-    start: function() {
+    canvas : document.createElement("canvas"),
+    start : function() {
         this.canvas.width = 480;
         this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 20);
-    },
-    clear: function() {
+        },
+    clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
-    stop: function() {
+    stop : function() {
         clearInterval(this.interval);
     }
 }
 
 function component(width, height, color, x, y, type) {
     this.type = type;
-    if (type == "image") {
+    if (type == "image" || type == "background") {
         this.image = new Image();
         this.image.src = color;
     }
     this.width = width;
     this.height = height;
     this.speedX = 0;
-    this.speedY = 0;
+    this.speedY = 0;    
     this.x = x;
-    this.y = y;
+    this.y = y;    
     this.update = function() {
         context = myGameArea.context;
-        if (type == "image") {
-            context.drawImage(this.image,
-                this.x,
+        if (type == "image" || type == "background") {
+            context.drawImage(this.image, 
+                this.x, 
                 this.y,
                 this.width, this.height);
+        if (type == "background") {
+            context.drawImage(this.image, 
+                this.x + this.width, 
+                this.y,
+                this.width, this.height);
+        }
         } else {
             context.fillStyle = color;
             context.fillRect(this.x, this.y, this.width, this.height);
@@ -68,48 +64,32 @@ function component(width, height, color, x, y, type) {
     this.newPos = function() {
         this.x += this.speedX;
         this.y += this.speedY;
-    }
+        if (this.type == "background") {
+            if (this.x == -(this.width)) {
+                this.x = 0;
+            }
+        }
+    }    
 }
 
 function updateGameArea() {
-    console.log("Updating")
     myGameArea.clear();
-    myGamePiece.newPos();
+    myBackground.newPos();    
+    myBackground.update();
+    myGamePiece.newPos();    
     myGamePiece.update();
 }
 
+function move(dir) {
+    myGamePiece.image.src = "../assets/images/snail.png";
+    if (dir == "up") {myGamePiece.speedY = -1; }
+    if (dir == "down") {myGamePiece.speedY = 1; }
+    if (dir == "left") {myGamePiece.speedX = -1; }
+    if (dir == "right") {myGamePiece.speedX = 1; }
+}
 
 function clearmove() {
-    myGamePiece.speedX = 0;
-    myGamePiece.speedY = 0;
-}
-var mazeImg = new Image();
-var playerImg = new Image();
-
-mazeImg.onload = function() {
-    mazeImg.src = "../assets/images/maze.png";
-    context.drawImage(mazeImg, 0, 0, imageWidth, imageHeight, 0, 0, canvasWidth, canvasHeight);
-    context.beginPath();
-    context.arc(542, 122, 7, 0, 2 * Math.PI, false);
-    context.closePath();
-    context.fillStyle = '#00FF00';
-    context.fill();
-    
-
-};
-
-function moveup() {
-    myGamePiece.speedY = -1;
-}
-
-function movedown() {
-    myGamePiece.speedY = 1;
-}
-
-function moveleft() {
-    myGamePiece.speedX = -1;
-}
-
-function moveright() {
-    myGamePiece.speedX = 1;
+    myGamePiece.image.src = "../assets/images/player.png";
+    myGamePiece.speedX = 0; 
+    myGamePiece.speedY = 0; 
 }
