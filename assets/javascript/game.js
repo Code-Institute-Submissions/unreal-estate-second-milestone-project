5ccfd36d2681ec7025b1fd4eec8c3e5b283632f5
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-03-29 16:54:20 +0000

    span class close visible

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -169,15 +169,16 @@ function startInsaneLevel(){
 }
 
 //Feedback button
-var modal = document.getElementById("modal");
+var modal = document.getElementsByClassName("modal-content");
 var btn = document.getElementById("FeedbackBtn");
 var span = document.getElementsByClassName("close")[0];
-
-
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
 btn.addEventListener("click", modal.style.display = "block");
-span.addEventListener("click", modal.style.display = "none");
-
-
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
