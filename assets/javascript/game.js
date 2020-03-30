fabf534f946b440cd04c47e68b6a0d979afcc18d
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-03-30 15:47:48 +0000

    newpos if added, canMove function fixed to avoid slug hitting the walls

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,15 +13,15 @@ window.onload = function () {
 
 function startGame() {
     console.log("did anything");
-    myGamePiece = new component(28, 28, "./assets/images/player.png", 665, 516, "image");
+    myGamePiece = new component(15, 15, "./assets/images/player.png", 665, 516, "image");
     myBackground = new component(715, 570, "./assets/images/maze.png", 0, 0, "background");
     myDestination = new component(35, 35, "./assets/images/destination.png", 0, 25, "image");
     myGameArea.start();
 }
 
 var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
+    canvas: document.createElement("canvas"),
+    start: function () {
         this.canvas.width = 715;
         this.canvas.height = 570;
         this.canvas.style.left = 20;
@@ -30,12 +30,12 @@ var myGameArea = {
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 20);
-        },
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height, this.canvas.style.left, this.canvas.style.top );
+    },
+    clear: function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height, this.canvas.style.left, this.canvas.style.top);
     }
-     
-    
+
+
 };
 
 function component(width, height, color, x, y, type) {
@@ -47,80 +47,80 @@ function component(width, height, color, x, y, type) {
     this.width = width;
     this.height = height;
     this.speedX = 0;
-    this.speedY = 0;    
+    this.speedY = 0;
     this.x = x;
-    this.y = y;    
-    this.update = function() {
+    this.y = y;
+    this.update = function () {
         context = myGameArea.context;
         if (type == "image" || type == "background") {
-            context.drawImage(this.image, 
-                this.x, 
+            context.drawImage(this.image,
+                this.x,
                 this.y,
                 this.width, this.height);
-        if (type == "background") {
-            context.drawImage(this.image, 
-                this.x + this.width, 
-                this.y,
-                this.width, this.height);
-        }
+            if (type == "background") {
+                context.drawImage(this.image,
+                    this.x + this.width,
+                    this.y,
+                    this.width, this.height);
+            }
         } else {
             context.fillStyle = color;
             context.fillRect(this.x, this.y, this.width, this.height);
         }
-        
+
     };
-    this.newPos = function() {
-        this.x += this.speedX;
+    this.newPos = function () {
+        if (1 === canMoveTo(this.x + this.speedX, this.y + this.speedY, myGameArea.context)){
+             this.x += this.speedX;
         this.y += this.speedY;
+        } 
+        
         if (this.type == "background") {
             if (this.x == -(this.width)) {
                 this.x = 0;
             }
         }
-    };  
-    
+    };
+
 }
 
 function updateGameArea() {
     myGameArea.clear();
-    myBackground.newPos();    
+    myBackground.newPos();
     myBackground.update();
-    myGamePiece.newPos();    
+    myGamePiece.newPos();
     myGamePiece.update();
-    myDestination.newPos(); 
+    myDestination.newPos();
     myDestination.update();
 }
 let newX;
 let newY;
-function canMoveTo(destX, destY,context) { 
+function canMoveTo(destX, destY, context) {
     console.log("0");
     var imgData = context.getImageData(destX, destY, 40, 35);
     console.log("1");
     var data = imgData.data; console.log("2");
     var canMove = 1;
     console.log("3");
-    if (destX >= 0 && destX <= imageWidth - 15 && destY >= 0 && destY <= imageHeight - 15) { 
-    console.log("4");
+    if (destX >= 0 && destX <= imageWidth - 15 && destY >= 0 && destY <= imageHeight - 15) {
+        console.log("4");
         for (var i = 0; i < 4 * 40 * 35; i += 4) { console.log("5");
             if (data[i] === 0 && data[i + 1] === 0 && data[i + 2] === 0) { 
-            console.log("6");
                 canMove = 0;
-                console.log("7");
                 break;
-                console.log("8");
             } 
-            else if (data[i] === 93 && data[i + 1] === 188 && data[i + 2] === 210) { //the colour of the destination
-                 console.log("9");
-                canMove = 2; 
-                console.log("10");
+            else { //the colour of the destination
+                console.log(data[i], data[i+1], data[i+2])
+                canMove = 1; 
                 break; 
-                console.log("11");
             }
         }
+
     }
     else {
+        console.log("Can not move");
         canMove = 0;
-         console.log("12");
+        console.log("12");
     }
     return canMove;
     console.log("13");
@@ -132,18 +132,29 @@ function move(dir) {
     var newY;
     var canMove; //direction buttons
     if (dir ==="up") {
-        if (1 === canMoveTo(myGamePiece.x, myGamePiece.y-1, myGameArea.context));
-        myGamePiece.speedY = -1}        
-    if (dir === "down"){
-        if (1 == canMoveTo(myGamePiece.x, myGamePiece.y+1,myGameArea.context));
-    } {myGamePiece.speedY = 1; }
-    if (dir === "left"){
-        if (1 == canMoveTo(myGamePiece.x-1, myGamePiece.y, myGameArea.context));
-    } {myGamePiece.speedX = -1; }
-    if (dir === "right"){
-        if (1 == canMoveTo(myGamePiece.x+1, myGamePiece.y, myGameArea.context));
-    } {myGamePiece.speedX = 1; }
+        if (1 === canMoveTo(myGamePiece.x, myGamePiece.y-1, myGameArea.context)){
+            myGamePiece.speedY = -1;
+        }  
     }
+    if (dir === "down"){
+        if (1 == canMoveTo(myGamePiece.x, myGamePiece.y+1,myGameArea.context)){
+            myGamePiece.speedY = 1;
+        }
+    } 
+    if (dir === "left"){
+        console.log(canMoveTo(myGamePiece.x-1, myGamePiece.y, myGameArea.context));
+        if (1 == canMoveTo(myGamePiece.x-1, myGamePiece.y, myGameArea.context)){ 
+            myGamePiece.speedX = -1; 
+        }else{
+            console.log("Could not move");
+        }
+    }
+    if (dir === "right"){
+        if (1 == canMoveTo(myGamePiece.x+1, myGamePiece.y, myGameArea.context)){
+            myGamePiece.speedX = 1; 
+        }
+    }
+}
 
 function clearmove() {
     myGamePiece.image.src = "../assets/images/player.png";
@@ -154,18 +165,18 @@ function clearmove() {
 
 //select level
 
-function startEasyLevel(){
-     myBackground = new component(715, 570, "./assets/images/maze.png", 0, 0, "background");
+function startEasyLevel() {
+    myBackground = new component(715, 570, "./assets/images/maze.png", 0, 0, "background");
 
 }
-function startMediumLevel(){
-    myBackground = new component(715, 570, "./assets/images/medium.png", 0, 0, "background"); 
+function startMediumLevel() {
+    myBackground = new component(715, 570, "./assets/images/medium.png", 0, 0, "background");
 }
-function startHardLevel(){
-     myBackground = new component(715, 570, "./assets/images/hard.png", 0, 0, "background");
+function startHardLevel() {
+    myBackground = new component(715, 570, "./assets/images/hard.png", 0, 0, "background");
 }
-function startInsaneLevel(){
-     myBackground = new component(715, 570, "./assets/images/insane.png", 0, 0, "background");
+function startInsaneLevel() {
+    myBackground = new component(715, 570, "./assets/images/insane.png", 0, 0, "background");
 }
 
 //Feedback button
@@ -173,11 +184,11 @@ var span = document.getElementsByClassName("close")[0];
 
 
 function sendFeedback() {
-  var x = document.getElementById("modal");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } 
-  else {
-    x.style.display = "none";
-  }
+    var x = document.getElementById("modal");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
 }
