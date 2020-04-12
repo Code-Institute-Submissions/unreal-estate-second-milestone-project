b71abf298f3c6e0aadb784b3dde19705ef58e16d
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-04-12 12:41:44 +0000

    finish message added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,7 @@ var canvasWidth = 715;
 var canvasHeight = 570;
 
 
-window.onload = function () {
+window.onload = function() {
     startGame();
 }
 
@@ -21,7 +21,7 @@ function startGame() {
 
 var myGameArea = {
     canvas: document.createElement("canvas"),
-    start: function () {
+    start: function() {
         this.canvas.width = 715;
         this.canvas.height = 570;
         this.canvas.style.left = 20;
@@ -31,7 +31,7 @@ var myGameArea = {
         this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 20);
     },
-    clear: function () {
+    clear: function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height, this.canvas.style.left, this.canvas.style.top);
     }
 
@@ -50,7 +50,7 @@ function component(width, height, color, x, y, type) {
     this.speedY = 0;
     this.x = x;
     this.y = y;
-    this.update = function () {
+    this.update = function() {
         context = myGameArea.context;
         if (type == "image" || type == "background") {
             context.drawImage(this.image,
@@ -69,7 +69,7 @@ function component(width, height, color, x, y, type) {
         }
 
     };
-    this.newPos = function () {
+    this.newPos = function() {
         if (1 === canMoveTo(this.x + this.speedX, this.y + this.speedY, myGameArea.context)) {
             this.x += this.speedX;
             this.y += this.speedY;
@@ -95,6 +95,7 @@ function updateGameArea() {
 }
 let newX;
 let newY;
+
 function canMoveTo(destX, destY, context) {
 
     var imgData = context.getImageData(destX, destY, 40, 35);
@@ -109,119 +110,123 @@ function canMoveTo(destX, destY, context) {
             if (data[i] === 0 && data[i + 1] === 0 && data[i + 2] === 0) {
                 canMove = 0;
                 break;
-            }
-
-            else {
+            } else {
                 data[i], data[i + 1], data[i + 2]
                 canMove = 1;
                 break;
+            } else if {
+                (data[i] === 221 && data[i + 1] === 179 && data[i + 2] === 116)
+                canMove = 2;
+                break;
+                youWon();
             }
 
-            /* else if { 
-             (data[i] === 221 && data[i + 1] === 179 && data[i + 2] === 116)
-             canMove =2;
-             break;
-             alert("Game over", response);
-                 
-             }*/
+            }
+        }
+    
+
+    function move(dir) {
+        var newX;
+        var newY;
+        var canMove; //direction buttons
+        if (dir === "up") {
+            if (1 === canMoveTo(myGamePiece.x, myGamePiece.y - 1, myGameArea.context)) {
+                myGamePiece.speedY = -1;
+            }
+        }
+        if (dir === "down") {
+            if (1 == canMoveTo(myGamePiece.x, myGamePiece.y + 1, myGameArea.context)) {
+                myGamePiece.speedY = 1;
+            }
+        }
+        if (dir === "left") {
+            console.log(canMoveTo(myGamePiece.x - 1, myGamePiece.y, myGameArea.context));
+            if (1 == canMoveTo(myGamePiece.x - 1, myGamePiece.y, myGameArea.context)) {
+                myGamePiece.speedX = -1;
+            } else {
+                console.log("Could not move");
+            }
+        }
+        if (dir === "right") {
+            if (1 == canMoveTo(myGamePiece.x + 1, myGamePiece.y, myGameArea.context)) {
+                myGamePiece.speedX = 1;
+            }
         }
     }
-    else {
-        console.log("Can not move");
-        canMove = 0;
 
+    function clearmove() {
+        myGamePiece.image.src = "../assets/images/player.png";
+        myGamePiece.speedX = 0;
+        myGamePiece.speedY = 0;
     }
-    return canMove;
-
-}
 
 
+    //select level
 
-function move(dir) {
-    var newX;
-    var newY;
-    var canMove; //direction buttons
-    if (dir === "up") {
-        if (1 === canMoveTo(myGamePiece.x, myGamePiece.y - 1, myGameArea.context)) {
-            myGamePiece.speedY = -1;
-        }
+    function startEasyLevel() {
+        myBackground = new component(715, 570, "./assets/images/maze.png", 0, 0, "background");
+        myGamePiece = new component(30, 25, "./assets/images/player.png", 665, 516, "image");
+        myDestination = new component(35, 35, "./assets/images/destination.png", 0, 25, "image");
+        myGameArea.start();
     }
-    if (dir === "down") {
-        if (1 == canMoveTo(myGamePiece.x, myGamePiece.y + 1, myGameArea.context)) {
-            myGamePiece.speedY = 1;
-        }
+
+    function startMediumLevel() {
+        myBackground = new component(715, 570, "./assets/images/medium.png", 0, 0, "background");
+        myGamePiece = new component(22, 15, "./assets/images/player.png", 680, 540, "image");
+        myDestination = new component(35, 35, "./assets/images/destination.png", 0, 15, "image");
+        myGameArea.start();
     }
-    if (dir === "left") {
-        console.log(canMoveTo(myGamePiece.x - 1, myGamePiece.y, myGameArea.context));
-        if (1 == canMoveTo(myGamePiece.x - 1, myGamePiece.y, myGameArea.context)) {
-            myGamePiece.speedX = -1;
+
+    function startHardLevel() {
+        myBackground = new component(715, 570, "./assets/images/hard.png", 0, 0, "background");
+        myGamePiece = new component(20, 15, "./assets/images/player.png", 680, 540, "image");
+        myDestination = new component(25, 25, "./assets/images/destination.png", 0, 15, "image");
+        myGameArea.start();
+    }
+
+    function startInsaneLevel() {
+        myBackground = new component(715, 570, "./assets/images/insane.png", 0, 0, "background");
+        myGamePiece = new component(20, 15, "./assets/images/player.png", 685, 540, "image");
+        myDestination = new component(25, 25, "./assets/images/destination.png", 0, 10, "image");
+        myGameArea.start();
+    }
+
+    //Destination reached, game over
+    //Method based on https://github.com/TheCodeDepository/PickleRick-MazeGame
+    function youWon() {
+        document.getElementById('moves').innerHtml = "You moved" + moves + "steps.";
+        toggleVisability("finishMessage");
+        document.getElementById("okBtn").focus();
+    }
+
+    function toggleVisablity(id) {
+        if (document.getElementById(id).style.visibility == "visible") {
+            document.getElementById(id).style.visibility = "hidden";
         } else {
-            console.log("Could not move");
+            document.getElementById(id).style.visibility = "visible";
         }
     }
-    if (dir === "right") {
-        if (1 == canMoveTo(myGamePiece.x + 1, myGamePiece.y, myGameArea.context)) {
-            myGamePiece.speedX = 1;
-        }
+
+    //Feedback button
+    // Submit and send mail with email.js
+
+
+    function sendMail(feedbackForm) {
+        emailjs.init("user_UW97WmP3GsBepuyB8Vffd");
+        emailjs.send("gmail", "unreal_estate", {
+                "from_name": feedbackForm.name.value,
+                "from_email": feedbackForm.email.value,
+                "feedback": feedbackForm.feedback.value
+            })
+            .then(
+                function(response) {
+                    alert("Thanks for your opinion!", response);
+                    $('#myModal').modal('hide');
+                },
+                function(error) {
+                    alert("Failed", error);
+                    $('#myModal').modal('hide');
+                }
+            );
+        return false;
     }
-}
-
-function clearmove() {
-    myGamePiece.image.src = "../assets/images/player.png";
-    myGamePiece.speedX = 0;
-    myGamePiece.speedY = 0;
-}
-
-
-//select level
-
-function startEasyLevel() {
-    myBackground = new component(715, 570, "./assets/images/maze.png", 0, 0, "background");
-    myGamePiece = new component(30, 25, "./assets/images/player.png", 665, 516, "image");
-    myDestination = new component(35, 35, "./assets/images/destination.png", 0, 25, "image");
-    myGameArea.start();
-}
-function startMediumLevel() {
-    myBackground = new component(715, 570, "./assets/images/medium.png", 0, 0, "background");
-    myGamePiece = new component(22, 15, "./assets/images/player.png", 680, 540, "image");
-    myDestination = new component(35, 35, "./assets/images/destination.png", 0, 15, "image");
-    myGameArea.start();
-}
-function startHardLevel() {
-    myBackground = new component(715, 570, "./assets/images/hard.png", 0, 0, "background");
-    myGamePiece = new component(20, 15, "./assets/images/player.png", 680, 540, "image");
-    myDestination = new component(25, 25, "./assets/images/destination.png", 0, 15, "image");
-    myGameArea.start();
-}
-function startInsaneLevel() {
-    myBackground = new component(715, 570, "./assets/images/insane.png", 0, 0, "background");
-    myGamePiece = new component(20, 15, "./assets/images/player.png", 685, 540, "image");
-    myDestination = new component(25, 25, "./assets/images/destination.png", 0, 10, "image");
-    myGameArea.start();
-}
-
-
-
-//Feedback button
-// Submit and send mail with email.js
-
-
-function sendMail(feedbackForm) {
-    emailjs.init("user_UW97WmP3GsBepuyB8Vffd");
-    emailjs.send("gmail", "unreal_estate", {
-        "from_name": feedbackForm.name.value,
-        "from_email": feedbackForm.email.value,
-        "feedback": feedbackForm.feedback.value
-    })
-        .then(
-            function (response) {
-                alert("Thanks for your opinion!", response);
-                $('#myModal').modal('hide');
-            },
-            function (error) {
-                alert("Failed", error);
-                $('#myModal').modal('hide');
-            }
-        );
-    return false;
-}
